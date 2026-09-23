6afca9c48f9bec88909a9a1ddd80b2957d4e004d
Suyoung <sueyoungkim69@gmail.com>
2026-09-23 23:24:25 +0900

    spring initializer 설정

---
+++ src/main/java/com/example/chuseok_coding/ChuseokCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.chuseok_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuseokCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuseokCodingApplication.class, args);
+	}
+
+}
