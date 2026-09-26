308c6b48f768d13f45e9ac6aaebba44f21f3ce0f
Suyoung <sueyoungkim69@gmail.com>
2026-09-26 18:21:13 +0900

    @ResponseBody 사용

--- src/main/java/com/example/chuseok_coding/controller/IndexController.java
+++ src/main/java/com/example/chuseok_coding/controller/IndexController.java
@@ -3,9 +3,12 @@ package com.example.chuseok_coding.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    //HTML 반환이 아닌 JSON 반환
+    @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
