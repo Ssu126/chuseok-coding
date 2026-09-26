92d900d63486e90012aff1e843170344b4860ab3
Suyoung <sueyoungkim69@gmail.com>
2026-09-26 18:47:13 +0900

    @RestController 사용 (5-13 ~ 5-24)

--- src/main/java/com/example/chuseok_coding/controller/IndexController.java
+++ src/main/java/com/example/chuseok_coding/controller/IndexController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
     //HTML 반환이 아닌 JSON 반환
-    @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
