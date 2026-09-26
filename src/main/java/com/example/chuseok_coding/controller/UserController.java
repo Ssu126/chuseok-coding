81bd59bf7ac8e0073927d77302a2a118f48b8649
Suyoung <sueyoungkim69@gmail.com>
2026-09-26 14:51:36 +0900

    UserController를 String에서 ModelAndView로 수정

--- src/main/java/com/example/chuseok_coding/controller/UserController.java
+++ src/main/java/com/example/chuseok_coding/controller/UserController.java
@@ -1,18 +1,29 @@
 package com.example.chuseok_coding.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public String userPage() {
-        return "/users/list";
+    public ModelAndView userPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/users/list");
+        return modelAndView;
     }
 
     @RequestMapping(value = "/users/1/detail", method = RequestMethod.GET)
-    public String detailPage() {
-        return "/users/detail";
+    public ModelAndView detailPage(Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("id", 1);
+        modelAndView.addObject("name", "Susu");
+        modelAndView.addObject("age", 10);
+        modelAndView.addObject("job", "Developer");
+        modelAndView.addObject("specialty", "Backend");
+        modelAndView.setViewName("/users/detail");
+        return modelAndView;
     }
 }
