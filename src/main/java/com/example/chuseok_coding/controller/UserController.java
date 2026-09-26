313d6184740e4aa4adedc36b156ce24d1fd669b8
Suyoung <sueyoungkim69@gmail.com>
2026-09-26 18:19:53 +0900

    Model(인터페이스) + String 사용

--- src/main/java/com/example/chuseok_coding/controller/UserController.java
+++ src/main/java/com/example/chuseok_coding/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.chuseok_coding.service.UserServiceInterface;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,14 @@ public class UserController {
     private UserServiceInterface AUserService;
 
     @GetMapping
-    public String userPage(ModelMap model) {
+    public String userPage(Model model) {
         List<User> users = AUserService.findAll();
         model.addAttribute("users", users);
         return "/users/list";
     }
 
     @GetMapping(value = "/1/detail")
-    public String detailPage(ModelMap model) {
+    public String detailPage(Model model) {
         User user = AUserService.findById(1);
         model.addAttribute("id", user.getId());
         model.addAttribute("name", user.getName());
